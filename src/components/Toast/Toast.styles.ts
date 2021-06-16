80c58d2e7d5b09b24b4aa1103ffe2db9dac9365b
Quinn Vaughn <qvaughn3@gmail.com>
2021-06-16 13:41:36 -0500

    Removed cursor from toast.

--- src/components/Toast/Toast.styles.ts
+++ src/components/Toast/Toast.styles.ts
@@ -10,7 +10,6 @@ const StyledToast = styled.div<StyledToastProps>`
     p.toastType === "success"
       ? p.theme.colors.toast.success
       : p.theme.colors.toast.error};
-  cursor: pointer;
   font-size: ${(p) => p.theme.sizes.md};
   padding: ${(p) => p.theme.sizes.md};
   color: ${(p) => p.theme.colors.text.white};
